2bd9d88a60e05cb757fc87f354a964a3630834d8
Adibas03 <adibas03@ymail.com>
2018-05-06 01:19:34 +0100

    add wallet style will draft

---
+++ contracts/Wallet.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.23;
+
+import './Ownable.sol';
+
+/**
+* Wallet instantiation, is albe to run almost any Interaction: eth, toke, transactions
+*/
+contract Wallet is Ownable {
+
+  constructor ()
+    public
+  {
+  }
+
+  function transfer (address _destination, uint256 _value)
+    public onlyOwner
+  returns (bool) {
+    _destination.transfer(_value);
+    return true;
+  }
+
+  function transfer ()
+    public payable
+  returns (bool) {
+    return true;
+  }
+
+  function callFunction (address _address, uint256 _value, bytes32 _callData) //Can be used to make wallet type calls, to interact with smart contracts
+    public payable onlyOwner
+  returns (bool) {
+    return _address.call.value(_value)(_callData);
+  }
+
+  function ()
+    public payable
+  {
+  }
+}
