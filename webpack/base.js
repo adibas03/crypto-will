4ea8e4f5399b8c357a57d9fc29c936a37ffa13f3
adibas03 <adibas03@ymail.com>
2019-12-15 23:07:29 +0100

    test

--- webpack/base.js
+++ webpack/base.js
@@ -1,81 +1,86 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const settings = {
-    entry: [
-        '@babel/polyfill',
-        './src/index.js'
-    ],
-    output: {
-        filename: 'js/[name].js',
-        publicPath: './',
-        path: path.resolve('dist')
-    },
-    resolve: {
-        extensions: ['.js', '.json']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                // exclude: path.resolve(__dirname, 'node_modules')
-            },
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                // exclude: path.resolve(__dirname, 'node_modules')
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            importLoaders: 1,
-                            plugins: () => [
-                                require('autoprefixer')({ browsers: ['last 3 versions'] }),
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                    }, {
-                        loader: "less-loader",
-                        options: {
-                            javascriptEnabled: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?)$/,
-                loader: 'url-loader?limit=10000',
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg|ttf|eot)$/,
-                loader: 'file-loader?name=[name].[ext]?[hash]',
-            },
+  entry: ["@babel/polyfill", "./src/index.js"],
+  output: {
+    filename: "js/[name].js",
+    publicPath: "./",
+    path: path.resolve("dist")
+  },
+  resolve: {
+    extensions: [".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+        // exclude: path.resolve(__dirname, 'node_modules')
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+        // exclude: path.resolve(__dirname, 'node_modules')
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              importLoaders: 1,
+              plugins: () => [
+                require("autoprefixer")({ browsers: ["last 3 versions"] })
+              ]
+            }
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Crypto Will Dapp',
-            filename: 'index.html'
-        })
-    ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "less-loader",
+            options: {
+              javascriptEnabled: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?)$/,
+        loader: "url-loader?limit=10000"
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|ttf|eot)$/,
+        loader: "file-loader?name=[name].[ext]?[hash]"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Crypto Will Dapp",
+      filename: "index.html",
+      template: "src/www/index.html"
+    }),
+    new CopyPlugin([
+      {
+        from: "src/www/404.html"
+      }
+    ])
+  ]
 };
 
 module.exports = settings;
