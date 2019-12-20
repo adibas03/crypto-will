ef67d6e7001c06114b6ea58afdd0644d5fc89216
adibas03 <adibas03@ymail.com>
2019-12-20 01:23:47 +0100

    fix test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,5 @@ var DeployerLibrary = artifacts.require("./DeployerLibrary.sol");
 module.exports = function(deployer, network) {
   deployer.deploy(DeployerLibrary);
   deployer.link(DeployerLibrary, Deployer);
-  if (network === "test") {
-    return;
-  }
   deployer.deploy(Deployer);
 };
